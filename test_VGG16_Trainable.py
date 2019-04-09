2d0835e55a7c9abe34d14d61cc884033a09bcdea
dat <datthangvan@gmail.com>
2019-04-10 03:00:29 +0700

    update

--- test_VGG16_Trainable.py
+++ test_VGG16_Trainable.py
@@ -2,10 +2,11 @@ import tensorflow as tf
 import numpy as np
 from VGG16_Trainable import *
 import os
-
+import skimage.io
+import matplotlib.pyplot as plt
 
 TEST_DATA_DIR = "./test_data"
-if os.path.isdir(TEST_DATA_DIR):
+if not os.path.isdir(TEST_DATA_DIR):
     print("{} is not folder".format(TEST_DATA_DIR))
 
 else:
@@ -14,4 +15,15 @@ else:
         os.mkdir(RESULT_DIR)
 
     files = os.listdir(TEST_DATA_DIR)
-    print(files)
+    img = []
+    for f in files:
+        file = os.path.join(os.getcwd(), TEST_DATA_DIR[2:])
+        file = os.path.join(file, f)
+
+        img.append(skimage.io.imread(file))
+
+    plt.subplot(1, 2, 1).set_title("first")
+    plt.imshow(img[0])
+    plt.subplot(1, 2, 2).set_title("second")
+    plt.imshow(img[1])
+
