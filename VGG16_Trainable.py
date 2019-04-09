64ae6683c0623abd83258a45e070379579586cfc
dat <datthangvan@gmail.com>
2019-04-10 02:40:58 +0700

    update

--- VGG16_Trainable.py
+++ VGG16_Trainable.py
@@ -40,6 +40,54 @@ class Vgg16:
 
         assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
 
+        self.conv1_1 = self.conv_layer(bgr, 3, 64, "conv1_1")
+        self.conv1_2 = self.conv_layer(self.conv1_1, 64, 64, "conv1_2")
+        self.poo1 = self.max_pool(self.conv1_2, "pool1")
+
+        self.conv2_1 = self.conv_layer(self.poo1, 64, 128, "conv2_1")
+        self.conv2_2 = self.conv_layer(self.conv2_1, 128, 128, "conv2_2")
+        self.poo2 = self.max_pool(self.conv2_2, "pool2")
+
+        self.conv3_1 = self.conv_layer(self.poo2, 128, 256, "conv3_1")
+        self.conv3_2 = self.conv_layer(self.conv3_1, 256, 256, "conv3_2")
+        self.conv3_3 = self.conv_layer(self.conv3_2, 256, 256, "conv3_3")
+        self.conv3_4 = self.conv_layer(self.conv3_3, 256, 256, "conv3_4")
+        self.poo3 = self.max_pool(self.conv3_4, "poo3")
+
+        self.conv4_1 = self.conv_layer(self.poo3, 256, 512, "conv4_1")
+        self.conv4_2 = self.conv_layer(self.conv4_1, 512, 512, "conv4_2")
+        self.conv4_3 = self.conv_layer(self.conv4_2, 512, 512, "conv4_3")
+        self.conv4_4 = self.conv_layer(self.conv4_3, 512, 512, "conv4_4")
+        self.poo4 = self.max_pool(self.conv4_4, "pool4")
+
+        self.conv5_1 = self.conv_layer(self.poo4, 512, 512, "conv5_1")
+        self.conv5_2 = self.conv_layer(self.conv5_1, 512, 512, "conv5_2")
+        self.conv5_3 = self.conv_layer(self.conv5_2, 512, 512, "conv5_2")
+        self.conv5_4 = self.conv_layer(self.conv5_3, 512, 512, "conv5_3")
+        self.poo5 = self.max_pool(self.conv5_4, "pool5")
+
+        self.fc6 = self.fc_layer(self.poo5, 25088, 4096, "fc6")
+        self.relu6 = tf.nn.relu(self.fc6)
+
+        if train_mode is False:
+            self.relu6 = tf.cond(train_mode, lambda: tf.nn.dropout(self.relu6, self.dropout), lambda : self.relu6)
+        else:
+            self.relu6 = tf.nn.dropout(self.relu6, self.dropout)
+
+        self.fc7 = self.fc_layer(self.fc6, 4096, 4096, "fc7")
+        self.relu7 = tf.nn.relu(self.fc7)
+
+        if train_mode is False:
+            self.relu7 = tf.cond(train_mode, lambda: tf.nn.dropout(self.relu7, self.dropout), lambda : self.relu7)
+        else:
+            self.relu7 = tf.nn.dropout(self.relu7, self.dropout)
+
+        self.fc8 = self.fc_layer(self.relu7, 4096, 1000, "fc8")
+
+        self.prob = tf.nn.softmax(self.fc8, name="prob")
+        self.data_dict = None
+
+
     def avg_pool(self, bottom, name):
         return tf.nn.avg_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
 
